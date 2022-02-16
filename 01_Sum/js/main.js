88e0d132b494b8ca32cce5c29a257fef27cabd08
cemal sezer <cemalsezer.dev@gmail.com>
2022-02-16 14:42:26 +0300

    first exercise is done

---
+++ 01_Sum/js/main.js
@@ -0,0 +1,24 @@
+/* example - 1 */
+let numFirst = 1;
+let numSecond= 2;
+let sum = numFirst+numSecond;
+alert(sum);
+
+/* example - 2 */
+let sum=0;
+let numFirst= Number(prompt("Birinci Sayi:"));
+let numSecond= Number(prompt("İkinci Sayi:"));
+sum= numFirst+numSecond
+document.write("Sum: ",sum);
+
+/* example - 3 */
+function sum(){
+    var numFirst =Number(document.getElementById("numFirst").value);
+    var numSecond =Number(document.getElementById("numSecond").value);
+    var sum = numFirst+ numSecond; 
+   document.getElementById("text").innerHTML=`Sum: ${sum}`;
+}
+
+
+
+
